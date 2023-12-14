fb67c0322292ec2194ad40b87a439a6a31daf2da
Rhuan Kowic <rhuanksantos2@gmail.com>
2023-12-14 09:41:49 -0300

    Criando repositorio dos carros.

---
+++ src/main/java/br/com/aluguelcarros/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package br.com.aluguelcarros.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.aluguelcarros.model.Car;
+
+public interface CarRepository extends JpaRepository<Car, Long>{
+  
+}
